0422a137723c506e7ba1c524fdf39d3dc0cba24b
Hurricane996 <levijwillrich@gmail.com>
2025-04-13 17:59:06 -0500

    copy to clipboard

--- static/index.js
+++ static/index.js
@@ -7,6 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
                 document.getElementById("token").value = object.token;
                 document.getElementById("timestamp").value = object.timestamp;
                 document.getElementById("time").value = object.time_formatted;
+
+
+                navigator.clipboard.writeText(`Timestamp: ${object.timestamp}; Token: ${object.token}`)
+                .then(function() {
+                    document.getElementById("form_results").innerHTML = `
+<div class="success">
+    <h3>Token copied to clipboard</h3>
+</div>`;
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })
                 
                 if (timeout !== undefined) {
                     clearTimeout(timeout)
